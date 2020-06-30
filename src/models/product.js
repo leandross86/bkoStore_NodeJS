462b31bf23ba6e65f9d6f75456a80e1721e43e13
leandross86 <leandroleandrossss@hotmail.com>
2020-06-30 09:16:39 -0300

    criando os models

---
+++ src/models/product.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Schema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    slug: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+        unique: true        
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        required: true,
+        default: true
+    },
+    tag: [{
+        type: String,
+        required: true
+    }]
+});
+
+module.exports = mongoose.model('Product', schema);
