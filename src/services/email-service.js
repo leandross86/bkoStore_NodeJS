52c889e0aeb7144411ca7a0bdc69b3c43b7cd6f4
Leandross86 <leandrobko@gmail.com>
2020-07-01 22:44:11 -0300

    corrigindo bug, arquivo de configuração e encriptando senha

--- src/services/email-service.js
+++ src/services/email-service.js
@@ -2,11 +2,13 @@
 let config = require('../config');
 let sendgrid = require('sendgrid')(config.sendgridKey);
 
+
 exports.send = async (to, subject, body) => {
     sendgrid.send({
         to: to,
-        from: '',
+        from: 'leandrossss@hotmail.com',
         subject: subject,
         html: body
     })
 }
+
