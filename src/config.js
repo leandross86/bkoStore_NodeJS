97f099ff9b081e24a1acc5ea1e186c83187ee85c
Leandross86 <leandrobko@gmail.com>
2020-07-02 22:07:08 -0300

    Enviando email de boas vindas

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
-global.EMAIL_TMPL = '<strong>{0}</strong>';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong> seja bem vindo à Node Store!';
 
 module.exports = {
     connectionString: 'mongodb+srv://bko:88159371@cluster0.liapp.mongodb.net/NodeStore?retryWrites=true&w=majorityNodeStore?retryWrites=true&w=majority',
