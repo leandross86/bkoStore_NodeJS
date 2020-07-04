d0e8e529d31ea95560c718c07aa09e7ceb34ed6b
Leandross86 <leandrobko@gmail.com>
2020-07-04 15:07:39 -0300

    refatorando3

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong> seja bem vindo à Node Store!';
 
 module.exports = {
-    connectionString: 'mongodb+srv://bko:88159371@cluster0.issoo.azure.mongodb.net/NodeStore?retryWrites=true&w=majority',
+    connectionString: 'SEU BANCO',
     sendgridKey: 'SUA CHAVE',
     containerConnectionString: 'SUA CONNETION STRING'
 
