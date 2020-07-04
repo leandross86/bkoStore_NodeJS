f2e2268651a4374c01718ab47583b489f0754451
leandross86 <43308192+leandross86@users.noreply.github.com>
2020-07-04 14:30:50 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong> seja bem vindo à Node Store!';
 
 module.exports = {
     connectionString: 'mongodb+srv://bko:88159371@cluster0.liapp.mongodb.net/NodeStore?retryWrites=true&w=majorityNodeStore?retryWrites=true&w=majority',
-    sendgridKey: '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',
+    sendgridKey: 'SUA CHAVE',
     containerConnectionString: 'TBD'
 }
